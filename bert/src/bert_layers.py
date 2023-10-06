0c4965ca67d0111ee3172cb9962b4cb4feb2bb49
jonsaadfalcon <jonsaadfalcon@gmail.com>
2023-10-06 14:19:58 -0700

    update

--- bert/src/bert_layers.py
+++ bert/src/bert_layers.py
@@ -1156,11 +1156,11 @@ class BertForSequenceClassification(BertPreTrainedModel):
                 loss_fct = nn.BCEWithLogitsLoss()
                 #loss_fct = nn.CrossEntropyLoss()
                 print("Logits and labels shapes")
-                print(logits)
+                #print(logits)
                 print(logits.shape)
-                print(labels)
+                #print(labels)
                 print(labels.shape)
-                loss = loss_fct(logits, labels)
+                loss = loss_fct(logits, labels.float())
                 assert False
 
         if not return_dict:
