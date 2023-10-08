2f672c157330381117475ffae1c8c69ad146f889
jonsaadfalcon <jonsaadfalcon@gmail.com>
2023-10-08 14:52:29 -0700

    update

--- bert/src/bert_layers.py
+++ bert/src/bert_layers.py
@@ -146,8 +146,8 @@ class BertEmbeddings(nn.Module):
             position_embeddings = self.position_embeddings(position_ids)
             for _ in range(1):
                 print("Two embedding shapes")
-                print(embeddings.weight.shape)
-                print(position_embeddings.weight.shape)
+                print(embeddings.shape)
+                print(position_embeddings.shape)
                 embeddings += position_embeddings
         
         embeddings = self.LayerNorm(embeddings)
