5566703da230d49f031493b0ad2fb0a771c8c90b
jonsaadfalcon <jonsaadfalcon@gmail.com>
2023-10-09 16:30:52 -0700

    update

--- bert/src/bert_layers.py
+++ bert/src/bert_layers.py
@@ -1107,7 +1107,7 @@ class BertForSequenceClassification(BertPreTrainedModel):
 
         ########################################
 
-        expand_positional_embeddings = True
+        expand_positional_embeddings = False
         if expand_positional_embeddings:
             
             original_embedding = model.bert.embeddings.position_embeddings
