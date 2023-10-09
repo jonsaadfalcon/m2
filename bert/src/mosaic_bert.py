847ff8154179cdf6437b676f64f444466d172c4d
jonsaadfalcon <jonsaadfalcon@gmail.com>
2023-10-09 16:27:36 -0700

    update

--- bert/src/mosaic_bert.py
+++ bert/src/mosaic_bert.py
@@ -321,7 +321,7 @@ def create_mosaic_bert_classification(
             metrics = [
                 #F1Score(task='multilabel', num_classes=num_labels, num_labels=num_labels, average='micro', threshold=0.),
                 #MultilabelAccuracy(num_labels=num_labels),
-                MultilabelF1Score(num_labels=num_labels, average='micro'),
+                MultilabelF1Score(num_labels=num_labels, average='micro', threshold=0.),
                 AUROC(task='multilabel', num_classes=num_labels, num_labels=num_labels, average='micro'),
             ]
         elif config.problem_type == 'single_label_classification':
