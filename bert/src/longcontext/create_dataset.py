00e185ae4b5f65f2d813b483aa776f2a452b2fa6
jonsaadfalcon <jonsaadfalcon@gmail.com>
2023-10-09 21:55:12 -0700

    update

--- bert/src/longcontext/create_dataset.py
+++ bert/src/longcontext/create_dataset.py
@@ -77,8 +77,8 @@ def create_mimic_dataset(split):
     def map_labels(example):
         labels = [0 for i in range(num_labels)]
         for label in example['labels']:
-            labels[label_map[label]] = 1
-        example['label_ids'] = torch.tensor(labels, dtype=torch.long)
+            labels[label_map[label]] = 1.0
+        example['label_ids'] = torch.tensor(labels, dtype=torch.float)
         #example['label'] = torch.tensor(labels, dtype=torch.long)
         del example['labels']
         return example
