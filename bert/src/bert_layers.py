fb38e367b06c3a6323121ee8e4a83aaa9b9ee3ef
jonsaadfalcon <jonsaadfalcon@gmail.com>
2023-10-10 16:20:40 -0700

    update

--- bert/src/bert_layers.py
+++ bert/src/bert_layers.py
@@ -1107,7 +1107,7 @@ class BertForSequenceClassification(BertPreTrainedModel):
 
         ########################################
 
-        expand_positional_embeddings = False
+        expand_positional_embeddings = True
         if expand_positional_embeddings:
             
             original_embedding = model.bert.embeddings.position_embeddings
@@ -1206,7 +1206,7 @@ class BertForSequenceClassification(BertPreTrainedModel):
         
         #pdb.set_trace()
 
-        expanding_contexts = False
+        expanding_contexts = True
         if expanding_contexts:
             assert self.bert.embeddings.position_embeddings.weight.shape[0] == 512
             assert self.bert.embeddings.position_embeddings.weight.shape[1] in [768, 960, 1568, 1792]
