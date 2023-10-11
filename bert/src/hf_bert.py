7f34f5ca815aa50cb21bdb4a22a363e03f8cf6a7
jonsaadfalcon <jonsaadfalcon@gmail.com>
2023-10-11 15:53:16 -0700

    update

--- bert/src/hf_bert.py
+++ bert/src/hf_bert.py
@@ -235,7 +235,7 @@ def create_hf_bert_classification(
             print("Adding metrics for multi_label_classification!")
             #print("Threshold: " + str(model_config['threshold']))
             metrics = [
-                MultilabelF1Score(num_labels=num_labels, average='micro', threshold=0.5),
+                MultilabelF1Score(num_labels=num_labels, average='micro', threshold=0.0),
                 #F1Score(task='multilabel', num_classes=num_labels, num_labels=num_labels, average='micro', threshold=0.0),
                 #AUROC(task='multilabel', num_classes=num_labels, num_labels=num_labels, average='micro'),
             ]
