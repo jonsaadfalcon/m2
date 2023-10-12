8442f9a0148a33f36de800320416d7ebb887f355
jonsaadfalcon <jonsaadfalcon@gmail.com>
2023-10-12 13:31:35 -0700

    update

--- bert/src/hf_bert.py
+++ bert/src/hf_bert.py
@@ -235,9 +235,9 @@ def create_hf_bert_classification(
             print("Adding metrics for multi_label_classification!")
             #print("Threshold: " + str(model_config['threshold']))
             metrics = [
-                MultilabelF1Score(num_labels=num_labels, average='micro', threshold=0.4),
+                #MultilabelF1Score(num_labels=num_labels, average='micro', threshold=0.4),
                 #F1Score(task='multilabel', num_classes=num_labels, num_labels=num_labels, average='micro', threshold=0.0),
-                #AUROC(task='multilabel', num_classes=num_labels, num_labels=num_labels, average='micro'),
+                AUROC(task='multilabel', num_classes=num_labels, num_labels=num_labels, average='micro'),
             ]
 
     return HuggingFaceModel(model=model,
