09617ac5470c6dddf5faacf92ff8a58d7fecef70
jonsaadfalcon <jonsaadfalcon@gmail.com>
2023-10-16 16:00:51 -0700

    update

--- bert/src/bert_layers.py
+++ bert/src/bert_layers.py
@@ -1183,6 +1183,7 @@ class BertForSequenceClassification(BertPreTrainedModel):
         print("model.bert.encoder.layer[10].attention.filter_fn2.pos_emb.t")
         print(model.bert.encoder.layer[10].attention.filter_fn2.pos_emb.t.shape)
         print(model.bert.encoder.layer[10].attention.filter_fn2.pos_emb.t)
+        assert False
 
         ########################################
 
